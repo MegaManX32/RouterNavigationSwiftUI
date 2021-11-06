206e76f03b6b4823c125388a5cab85b85da0b543
Vladislav Simovic <simke_deluxe@yahoo.co.uk>
2021-11-06 19:08:38 +0100

    Initial Commit

---
+++ RouterNavigationSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RouterNavigationSwiftUI
+//
+//  Created by Vladislav Simovic on 6.11.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RouterNavigationSwiftUI/RouterNavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterNavigationSwiftUIApp.swift
+//  RouterNavigationSwiftUI
+//
+//  Created by Vladislav Simovic on 6.11.21..
+//
+
+import SwiftUI
+
+@main
+struct RouterNavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
